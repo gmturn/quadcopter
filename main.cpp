3760f15873d21d83b969654729051ae3e6ebaa21
gmturn <turnermclean2402@gmail.com>
2022-10-17 11:11:46 -0500

    copied files from main directory

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include "time.h"
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+    double errorVal;
+    double proportionVal;
+    double integralVal;
+    double derivativeVal;
+
+    const double Kp = 0.0;
+    const double Ki = 0.0;
+    const double Kd = 0.0;
+
+
+
+
+
+    return 0;
+}
